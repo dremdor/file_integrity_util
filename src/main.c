fcf1f57a81bd8d15b81f0b0d6652564fd9c72b48
Manjular <dima220997999@gmail.com>
2024-07-18 18:18:55 +0300

    added check

--- src/main.c
+++ src/main.c
@@ -10,15 +10,17 @@
 
 void file_processing(const char file_name[256], const char *log_file);
 
+void sha256sum(const char file_name[256], char buffer[65]);
+
 void write_log(const char buffer[65], const char log_file[256], const char file_name[256]);
 
 void usage_message();
 
 void set(char *dir_name, const char *log_file);
 
-int check(char *dir_name, const char *log_file);
+int check(const char *log_file);
 
-void info_message(char *dir_name, char *log_file);
+void info_message(char *mode, char *dir_name, char *log_file);
 
 void check_format(int *format_flag, char *buffer);
 
@@ -26,13 +28,9 @@ int main(int argc, char **argv) {
     if (argc == 4) {
         if (strcmp(argv[1], "set") == 0) {
             set(argv[2], argv[3]);
-            info_message(argv[2], argv[3]);
+            info_message(argv[1], argv[2], argv[3]);
         } else if (strcmp(argv[1], "check") == 0) {
-            if (check(argv[2], argv[3]) == 1)
-                printf("all good");
-            else
-                printf("all bad");
-            // info_message(argv[2], argv[3]);
+            if (check(argv[3])) info_message(argv[1], argv[2], argv[3]);
         } else
             usage_message();
     } else
@@ -49,16 +47,23 @@ void check_format(int *format_flag, char *buffer) {
     if (!(buffer[64] == ' ' && buffer[65] == ' ')) *format_flag = 0;
 }
 
-void info_message(char *dir_name, char *log_file) {
-    syslog(LOG_INFO, "dir:%s integrity certified, created %s log file", dir_name, log_file);
-    printf("dir:%s integrity certified, created %s log file\n", dir_name, log_file);
+void info_message(char *mode, char *dir_name, char *log_file) {
+    syslog(LOG_INFO, "mode:%s dir:%s integrity certified from %s log file", mode, dir_name, log_file);
+    printf("mode:%s dir:%s integrity certified, from %s log file\n", mode, dir_name, log_file);
 }
 
 void file_processing(const char file_name[256], const char *log_file) {
+    char buffer[65];
+    sha256sum(file_name, buffer);
+    write_log(buffer, log_file, file_name);
+}
+
+void sha256sum(const char file_name[256], char buffer[65]) {
     FILE *fp;
     fp = fopen(file_name, "rb");
     if (fp == NULL) {
         syslog(LOG_ERR, "can't open %s file", file_name);
+        printf("can't open %s file\n", file_name);
         exit(1);
     }
     SHA256_CTX c;
@@ -70,15 +75,12 @@ void file_processing(const char file_name[256], const char *log_file) {
     }
     unsigned char sum[SHA256_DIGEST_LENGTH];
     SHA256_Final(sum, &c);
-    fclose(fp);
 
-    char buffer[65];
     for (int i = 0; i < 32; ++i) {
         sprintf(buffer + i * 2, "%02x", sum[i]);
     }
     buffer[64] = '\0';
-
-    write_log(buffer, log_file, file_name);
+    fclose(fp);
 }
 
 void write_log(const char buffer[65], const char log_file[256], const char file_name[256]) {
@@ -86,6 +88,7 @@ void write_log(const char buffer[65], const char log_file[256], const char file_
     tfp = fopen(log_file, "a+");
     if (tfp == NULL) {
         syslog(LOG_ERR, "can't open %s log file", log_file);
+        printf("can't open %s log file\n", log_file);
         exit(1);
     }
     fprintf(tfp, "%s  %s\n", buffer, file_name);
@@ -108,6 +111,7 @@ void set(char *dir_name, const char *log_file) {
     const struct dirent *entry;
     if (dp == NULL) {
         syslog(LOG_ERR, "can't open dir:%s", dir_name);
+        printf("can't open dir:%s\n", dir_name);
         exit(2);
     }
     while ((entry = readdir(dp)) != NULL) {
@@ -121,18 +125,35 @@ void set(char *dir_name, const char *log_file) {
     closedir(dp);
 }
 
-int check(char *dir_name, const char *log_file) {
+int check(const char *log_file) {
     int format_flag = 1;
     FILE *fp = fopen(log_file, "r");
     if (fp == NULL) {
         syslog(LOG_ERR, "can't open %s log file", log_file);
+        printf("can't open %s log file\n", log_file);
         exit(3);
     }
     char buffer[512];
-    while (fgets(buffer, sizeof(buffer), fp) != NULL && format_flag == 1) {
+    int err_flag = 1;
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
         check_format(&format_flag, buffer);
+        if (format_flag == 0) {
+            syslog(LOG_ERR, "incorrect format %s", log_file);
+            printf("incorrect format %s\n", log_file);
+            fclose(fp);
+            exit(4);
+        }
+        char sum_from_log[65];
+        char file_name[256];
+        sscanf(buffer, "%64s  %255s", sum_from_log, file_name);
+        char sum_from_file[65];
+        sha256sum(file_name, sum_from_file);
+        if (strcmp(sum_from_log, sum_from_file) != 0) {
+            syslog(LOG_ERR, "file:%s has been modified", log_file);
+            printf("file:%s has been modified\n", log_file);
+            err_flag = 0;
+        }
     }
     fclose(fp);
-    (void)dir_name;
-    return format_flag;
+    return err_flag;
 }
